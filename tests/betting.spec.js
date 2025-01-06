18ef8da9bfa0ca7a53ea97fd43c8f967d4d8b55c
mmuuripe <markus.muuripeal@gmail.com>
2025-01-06 15:43:55 +0200

    bruh

--- tests/betting.spec.js
+++ tests/betting.spec.js
@@ -12,7 +12,7 @@ test.describe('Epicbet search functionality tests', () => {
     test.beforeAll(() => {
         logger = new Logger('info');
     });
-    // WORKFLOW 
+    // TEST
     test.beforeEach(async ({ page }) => {
         mainPage = new Mainpage(page, logger);
         await mainPage.navigateTo('https://epicbet.com/en/');
